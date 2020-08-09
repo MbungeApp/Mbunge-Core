96dc6f4271334e5d1bef2190fdd7affb10a71fe4
Iampato <858wpwaweru@gmail.com>
2020-08-10 00:18:57 +0300

    minor fixes

--- models/db/Device.go
+++ models/db/Device.go
@@ -8,7 +8,7 @@ import (
 type Device struct {
 	ID        primitive.ObjectID `json:"id" bson:"_id"`
 	UserId    string             `json:"user_id" bson:"user_id"`
-	Type      string             `json:"type" bson:"user_id"`
+	Type      string             `json:"type" bson:"type"`
 	FCMToken  string             `json:"fcm_token" bson:"fcm_token"`
 	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
 	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
--- models/request/RegisterRequest.go
+++ models/request/RegisterRequest.go
@@ -1,8 +1,40 @@
 package request
 
-import "github.com/MbungeApp/mbunge-core/models/db"
+import (
+	"encoding/json"
+	"github.com/MbungeApp/mbunge-core/models/db"
+)
+
+func UnmarshalRegisterRequest(data []byte) (RegisterRequest, error) {
+	var r RegisterRequest
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *RegisterRequest) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
 
 type RegisterRequest struct {
-	Device db.Device
-	User   db.User
+	User     db.User `json:"user"`
+	Type     string  `json:"type" form:"type"`
+	FcmToken string  `json:"fcm_token" form:"fcm_token"`
 }
+
+/*
+{
+   "user":{
+      "first_name":"patrick",
+      "last_name":"waweru",
+      "email_address":"example@gmail.com",
+      "password":"123456789",
+      "phone_number":"123456789",
+      "county":"nakuru",
+      "date_birth":"iufidfdjfjd",
+      "gender":"1",
+      "profile_pic":"df"
+   },
+   "type":"mobile",
+   "fcm_token":"1234567dv"
+}
+*/
