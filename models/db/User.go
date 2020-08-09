ba3b2467b08b2a62382b9576646ace93349755c2
Iampato <858wpwaweru@gmail.com>
2020-08-09 18:31:38 +0300

    removed profile img from models

--- models/db/User.go
+++ models/db/User.go
@@ -13,10 +13,9 @@ type User struct {
 	Password     string             `json:"password" bson:"password" form:"password"`
 	PhoneNumber  string             `json:"phone_number" bson:"phone_number" form:"phone_number"`
 	DateBirth    time.Time          `json:"date_birth" bson:"date_birth" form:"date_birth"`
-	ProfilePic   string             `json:"profile_pic" bson:"profile_pic" form:"profile_pic"`
 	Gender       int                `json:"gender" bson:"gender" form:"gender"`
-	Verified     bool               `json:"verified" bson:"verified"`
-	County       string             `json:"county" bson:"county"`
-	CreatedAt    time.Time          `json:"created_at" bson:"created_at"`
-	UpdatedAt    time.Time          `json:"updated_at" bson:"updated_at"`
+	Verified     bool               `json:"verified" bson:"verified" form:"verified"`
+	County       string             `json:"county" bson:"county" form:"county"`
+	CreatedAt    time.Time          `json:"created_at" bson:"created_at" form:"created_at"`
+	UpdatedAt    time.Time          `json:"updated_at" bson:"updated_at" form:"updated"`
 }
