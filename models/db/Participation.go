63d4670747da9ace2fcb9c3c9482531ad2fa998a
Iampato <858wpwaweru@gmail.com>
2020-08-15 12:10:56 +0300

    added participation models

---
+++ models/db/Participation.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Participation struct {
+	ID        primitive.ObjectID `json:"id" bson:"_id"`
+	Name      string             `json:"name" bson:"name"`
+	Sector    string             `json:"sector" bson:"sector"`
+	Body      string             `json:"body" bson:"body"`
+	PostedBy  string             `json:"posted_by" bson:"posted_by"`
+	CreatedAt time.Time          `json:"created_at" bson:"created_at" form:"created_at"`
+	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at" form:"updated_at"`
+}
