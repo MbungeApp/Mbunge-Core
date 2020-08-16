867fd5c8c0a1bcc0cedd3cae73c1decd72432195
Iampato <858wpwaweru@gmail.com>
2020-08-16 12:10:05 +0300

    Added Participation models

--- models/db/Participation.go
+++ models/db/Participation.go
@@ -11,6 +11,7 @@ type Participation struct {
 	Sector    string             `json:"sector" bson:"sector"`
 	Body      string             `json:"body" bson:"body"`
 	PostedBy  string             `json:"posted_by" bson:"posted_by"`
+	ExpireAt  time.Time          `json:"expire_at" bson:"expire_at"`
 	CreatedAt time.Time          `json:"created_at" bson:"created_at" form:"created_at"`
 	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at" form:"updated_at"`
 }
