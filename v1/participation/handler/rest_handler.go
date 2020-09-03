b0518ae090c6efaaeedd858943ea16acbdd46a50
Iampato <858wpwaweru@gmail.com>
2020-09-03 10:16:00 +0300

    added websocket and rest participation handler layer

---
+++ v1/participation/handler/rest_handler.go
@@ -0,0 +1,66 @@
+package handler
+
+import (
+	"github.com/MbungeApp/mbunge-core/models/request"
+	"github.com/MbungeApp/mbunge-core/v1/participation/service"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	ms "github.com/mitchellh/mapstructure"
+	"log"
+	"net/http"
+)
+
+type participationRestHandler struct {
+	participationService service.ParticipationService
+}
+
+func NewParticipationRestHandler(e *echo.Echo, participationService service.ParticipationService) {
+	participationRestHandler := participationRestHandler{participationService: participationService}
+
+	// semantic versioning of api !!
+	g := e.Group("/api/v1/participation")
+	g.Use(middleware.JWT([]byte("secret")))
+	g.GET("/", participationRestHandler.AllParticipation)
+	g.GET("/response/:id", participationRestHandler.AllResponseByParticipation)
+	g.POST("/response/add", participationRestHandler.AddResponse)
+	g.DELETE("/response/delete/:id", participationRestHandler.DeleteResponse)
+}
+
+// Rest handlers
+func (p *participationRestHandler) AllParticipation(c echo.Context) error {
+	return c.JSON(http.StatusOK, p.participationService.GetAllParticipation())
+}
+func (p *participationRestHandler) AllResponseByParticipation(c echo.Context) error {
+	participationID := c.Param("id")
+	return c.JSON(http.StatusOK, p.participationService.GetAllResponseByParti(participationID))
+}
+func (p *participationRestHandler) AddResponse(c echo.Context) error {
+	var responseReq request.ResponseRequest
+	var info echo.Map
+	var err error
+
+	//1. bind the request payload to a struct
+	err = c.Bind(&info)
+	if err != nil {
+		return c.JSON(http.StatusConflict, err)
+	}
+	decodeConfig := ms.DecoderConfig{TagName: "json", Result: &responseReq}
+	decoder, err := ms.NewDecoder(&decodeConfig)
+	if err != nil {
+		log.Println(err)
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	err = decoder.Decode(&info)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err)
+	}
+	err = p.participationService.AddResponse(responseReq)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	return c.JSON(http.StatusCreated, "")
+}
+func (p *participationRestHandler) DeleteResponse(c echo.Context) error {
+	responseID := c.Param("response_id")
+	return c.JSON(http.StatusOK, p.participationService.DeleteResponse(responseID))
+}
---
+++ v1/participation/handler/ws_handler.go
@@ -0,0 +1,55 @@
+package handler
+
+import (
+	"context"
+	"fmt"
+	"github.com/MbungeApp/mbunge-core/v1/participation/service"
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo"
+	"net/http"
+)
+
+type participationWSHandler struct {
+	participationService service.ParticipationService
+}
+
+var (
+	upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
+
+func NewWebsocketHandler(e *echo.Echo, participationService service.ParticipationService) {
+	participationWSHandler := participationWSHandler{
+		participationService: participationService,
+	}
+	g := e.Group("/api/v1/participation")
+	g.GET("/ws", participationWSHandler.ChangeStreams)
+}
+
+// Websocket handler
+func (p *participationWSHandler) ChangeStreams(c echo.Context) error {
+	if c.IsWebSocket() {
+
+		ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+		if err != nil {
+			return err
+		}
+		defer ws.Close()
+
+		changes, err := p.participationService.ChangeStreams()
+		for changes.Next(context.TODO()) {
+			fmt.Println(changes.Current)
+			err = ws.WriteMessage(websocket.TextMessage, changes.Current)
+			if err != nil {
+				c.Logger().Error(err)
+			}
+		}
+
+	} else {
+		return c.JSON(http.StatusBadRequest, "websocket channel")
+	}
+	return nil
+}
