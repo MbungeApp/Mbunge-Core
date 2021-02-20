3cb5a9317e627ea55b53ce707ffe49b5390661bb
Iampato <858wpwaweru@gmail.com>
2021-02-20 21:29:06 +0300

    edit webinar stuff

--- models/request/add_webinar.go
+++ models/request/add_webinar.go
@@ -9,3 +9,12 @@ type AddWebinar struct {
 	Duration    int       `json:"duration" bson:"duration"`
 	ScheduleAt  time.Time `json:"schedule_at" bson:"schedule_at"`
 }
+
+type EditWebinar struct {
+	Agenda      string    `json:"agenda" bson:"agenda"`
+	HostedBy    string    `json:"hosted_by" bson:"hosted_by"`
+	Description string    `json:"description" bson:"description"`
+	Duration    int       `json:"duration" bson:"duration"`
+	Postponed   bool      `json:"postponed" bson:"postponed"`
+	ScheduleAt  time.Time `json:"schedule_at" bson:"schedule_at"`
+}
